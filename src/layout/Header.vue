976b25e704b66045a429eb7dc09fd459f63215e9
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2022-10-20 16:37:36 +0100

    started on heading for home

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,49 +1,57 @@
 <template>
-
   <header>
-
-    <base-nav class="navbar-main" id="navbar-main" type="transparent" effect="dark" expand>
-
-      <template v-slot:brand>
-
+    <base-nav
+      class="navbar-main"
+      id="navbar-main"
+      type="transparent"
+      effect="dark"
+      expand
+    >
+      <template v-slot:brand class="left">
         <router-link to="/" class="navbar-brand mr-lg-5">
           <brand-logo />
         </router-link>
-
       </template>
 
-      <template v-slot:content-header="{ closeMenu }">
+      <template v-slot:content-header="{ closeMenu }" class="right">
         <b-link @click="closeMenu">
           <i class="fa fa-times text-white" />
         </b-link>
       </template>
 
-      <ul class="navbar-nav navbar-nav-hover align-items-lg-center ml-lg-auto">
-
+      <ul
+        class="navbar-nav navbar-nav-hover align-items-lg-center ml-lg-auto right"
+      >
         <li class="nav-item">
           <router-link :to="{ name: 'index' }" custom v-slot="{ navigate }">
-            <b-button @click="navigate" role="link" class="nav-link" variant="link">
+            <b-button
+              @click="navigate"
+              role="link"
+              class="nav-link"
+              variant="link"
+            >
               Home
             </b-button>
           </router-link>
         </li>
 
         <base-dropdown class="nav-item" menu-classes="dropdown-menu-xl">
-
           <template v-slot:title>
             <router-link :to="{ name: 'about.story' }">
-              <button role="button" class="btn nav-link btn-link" data-toggle="dropdown">
+              <button
+                role="button"
+                class="btn nav-link btn-link"
+                data-toggle="dropdown"
+              >
                 <span class="nav-link-inner--text">About</span>
                 <i class="fa fa-chevron-down fa-lg" />
               </button>
             </router-link>
-
           </template>
 
           <template v-slot:default>
-
             <router-link :to="{ name: 'about.story' }" class="dropdown-item">
-              <i class="fa fa-info"  />
+              <i class="fa fa-info" />
               About us
             </router-link>
 
@@ -51,18 +59,20 @@
               <i class="fa-solid fa-users-line" />
               Team
             </router-link>
-            
+
             <router-link :to="{ name: 'about.contact' }" class="dropdown-item">
               <i class="fa fa-address-book" />
               Contact
             </router-link>
-            
-            <router-link :to="{ name: 'about.licensing' }" class="dropdown-item">
+
+            <router-link
+              :to="{ name: 'about.licensing' }"
+              class="dropdown-item"
+            >
               <i class="fa fa-file" />
               Licenses
             </router-link>
           </template>
-
         </base-dropdown>
 
         <li class="nav-item">
@@ -72,28 +82,36 @@
             </b-button>
           </router-link>
         </li>
-
       </ul>
-
     </base-nav>
-
   </header>
-
 </template>
 
-<script>
+<style lang="scss">
+.navbar-main {
+  padding: 0 200px;
+  display: flex;
+  flex-direction: row;
+}
 
-import BaseNav from '@/components/BaseNav'
-import BaseDropdown from '@/components/BaseDropdown'
+.left {
+  align-self: flex-start;
+}
+.right {
+  align-self: flex-end;
+}
+</style>
+
+<script>
+import BaseNav from "@/components/BaseNav";
+import BaseDropdown from "@/components/BaseDropdown";
 
 export default {
-
-  name: 'LayoutHeader',
+  name: "LayoutHeader",
 
   components: {
     BaseNav,
     BaseDropdown
   }
-
-}
+};
 </script>
--- src/views/Home/Section/Heading.vue
+++ src/views/Home/Section/Heading.vue
@@ -1,178 +1,162 @@
 <template>
-
   <div>
-
     <div class="introduction">
-
-      <div class="section-video">
-
-        <video autoplay muted loop>
-          <source src="/video/bg3.webm" type="video/mp4">
-        </video>
-
-      </div>
-
       <div class="intro-container">
-
         <div class="section-content">
+          <div class="intro-text">
+            <div class="m-auto">
+              <h3>Maximise Security ROI</h3>
 
-          <div>
-
-            <div class="intro-text">
-
-              <div class="box">
-
-                <div class="m-auto">
-
-                  <h2>
-                    Data<br>Insight
-                  </h2>
-
-                  <h3 class="text-danger">
-                    To maximise security ROI
-                  </h3>
-
-                </div>
-
-                <p class="lead mb-5">
-                  Combining security assets, intel, incidents, and more with threat-centric frameworks.
-                  We provide security insight allowing you to determine where to invest for maximum risk reduction.
-                </p>
-
-                <b-button @click="$router.push({ name: 'schedule.demo' })" variant="primary">
-                  Schedule A Demo
-                </b-button>
-
-              </div>
-
+              <h1>
+                A central exchange<br />to streamline the<br />cyber security<br />market.
+              </h1>
             </div>
 
-            <div class="intro-laptop">
+            <p class="lead mb-4">
+              Freedom from frustration, enabling vendors to align cyber security
+              solutions with consumer’s threat informed defence through
+              threat-centric methodology.
+            </p>
 
-              <device-laptop screen-css="bg-primary d-flex justify-content-start align-items-center text-white display-4">
-                <img src="@/assets/img/previews/technique-detail.jpg" height="100%">
-              </device-laptop>
-
+            <div
+              class="button"
+              @click="$router.push({ name: 'schedule.demo' })"
+            >
+              Book a Demo
             </div>
-
           </div>
 
+          <img
+            class="intro-laptop"
+            src="@/assets/img/previews/macbook.svg"
+            height="100%"
+          />
+          <div class="spotlight"></div>
         </div>
-
       </div>
-
+      <div class="see-more">See more<br />⌄</div>
     </div>
-
   </div>
-
 </template>
 
 <style lang="scss">
-
 .introduction {
-
   position: relative;
-  line-height: 0;
   clear: both;
-  background-color: slategrey;
-
-  .section-video {
-    overflow: hidden;
-
-    position: absolute;
-    top:0;
-    left:0;
-    right:0;
-
-    max-height: 730px;
-
-    video {
-      filter: contrast(150%);
-      min-height: 720px;
-      min-width: 100%;
-    }
-
-  }
+  padding: 200px 0;
+  padding-left: 200px;
 
   .intro-container {
     max-width: 1920px;
-    margin: auto;
+    min-height: calc(100vh - 400px);
+    margin: 0 auto;
   }
 
   .section-content {
-    position: relative;
-    height: 100%;
-    max-width: 1440px;
-    min-height: 720px;
-    margin: auto;
-    padding-bottom: 35%;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+
+    width: 100%;
+
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    justify-content: space-between;
   }
 
   .intro-text {
+    // position: absolute;
+    // top: 20%;
+    // left: 10%;
+    max-width: 540px;
+    text-align: left;
 
-    position: absolute;
-    top: 20%;
-    left: 10%;
-    max-width: 420px;
-    text-align: center;
-
-    h2 {
+    h1 {
       color: #fff;
+      font-weight: 600;
       font-size: 3rem;
+      line-height: 3.3rem;
       margin-bottom: 28px;
     }
 
     h3 {
-      font-size: 2.3rem;
-    }
-
-    h2, h3 {
-      display: inline-block;
-      margin-left: initial !important;
-      line-height: 1.0;
-      text-transform: uppercase;
-      padding: initial;
-      font-weight: bolder;
+      font-size: 1.3rem;
+      font-weight: 500;
+      color: var(--secondary);
     }
 
     p {
       color: #fff;
+      opacity: 0.6;
       font-weight: lighter;
       font-size: 1rem;
+      letter-spacing: 0.03rem;
     }
-
-    .box {
-
-      padding: 32px;
-      border-top-left-radius: 15px;
-      border-top-right-radius: 15px;
-
-    }
-
   }
 
   .intro-laptop {
-    position: absolute;
-    top: 30%;
-    left: 35%;
-    width: 90%;
-    max-width: 1280px;
+    // transform: translateX(300px);
+    // max-width: 1280px;
+    // height: 100%;
+    width: 800px;
     overflow: visible;
     z-index: 2;
   }
 
-  button {
-    width: 256px;
-    height: 64px;
-    border-radius: 32px;
+  .spotlight {
+    position: absolute;
+    top: 50%;
+    right: 0px;
+    width: 650px;
+    height: 37;
+    border-radius: 50%;
+    background: #5a349d;
+    z-index: -1;
+    box-shadow: 0px 0px 243px 308px rgba(90, 52, 157, 0.5);
   }
 
+  .button {
+    width: 170px;
+    height: 52px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    cursor: pointer;
+    font-weight: 600;
+    border-radius: 50px;
+    background-color: transparent;
+    border: var(--primary) solid 3px;
+    color: var(--primary);
+    text-align: unset;
+
+    transition: all 200ms ease;
+  }
+
+  .button:hover {
+    color: white;
+    background-color: var(--primary);
+    transform: scale(1.1);
+  }
+
+  .see-more {
+    position: absolute;
+    bottom: 48px;
+    left: 50%;
+    transform: translateX(-50%);
+
+    text-align: center;
+    color: #fff;
+    font-weight: lighter;
+    font-size: 1rem;
+    letter-spacing: 0.03rem;
+    line-height: 1.2rem;
+  }
 }
 
 @media only screen and (max-width: 1200px) {
-
   .introduction {
-
     .intro-container {
       position: relative;
       top: initial;
@@ -180,19 +164,17 @@
       right: initial;
 
       .section-content {
-        padding-bottom:5%;
+        padding-bottom: 5%;
         padding-top: 10%;
       }
     }
 
     .intro-text {
-
       position: relative;
       top: 0;
       left: 0;
       right: 0;
       margin: auto;
-
     }
 
     .intro-laptop {
@@ -212,17 +194,12 @@
         visibility: visible;
         height: inherit;
       }
-
     }
-
   }
-
 }
 
 @media only screen and (max-width: 720px) {
-
   .introduction {
-
     .intro-laptop {
       width: 100%;
     }
@@ -230,10 +207,7 @@
     .section-video {
       overflow: hidden;
       max-height: initial;
-
     }
-
   }
-
 }
 </style>
