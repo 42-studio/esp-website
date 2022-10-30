e6eb9a66b88f15836bbd85f69784906e71acbed7
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2022-10-30 16:38:45 +0000

    Added more pages

--- vue.config.js
+++ vue.config.js
@@ -19,8 +19,120 @@ module.exports = {
       entry: "src/main.js",
       // the source template
       template: "public/index.html",
-      // output as dist/index.html
+      // output as dist
       filename: "schedule-demo.html",
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "ES Profiler | Enterprise Security Measured",
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    },
+    "about/our-story": {
+      // entry for the page
+      entry: "src/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist
+      filename: "about/our-story.html",
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "ES Profiler | Enterprise Security Measured",
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    },
+    "about/our-team": {
+      // entry for the page
+      entry: "src/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist
+      filename: "about/our-team.html",
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "ES Profiler | Enterprise Security Measured",
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    },
+    "about/licensing": {
+      // entry for the page
+      entry: "src/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist
+      filename: "about/licensing.html",
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "ES Profiler | Enterprise Security Measured",
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    },
+    "about/getting-in-contact": {
+      // entry for the page
+      entry: "src/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist
+      filename: "about/getting-in-contact.html",
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "ES Profiler | Enterprise Security Measured",
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    },
+    "info/terms-conditions": {
+      // entry for the page
+      entry: "src/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist
+      filename: "info/terms-conditions.html",
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "ES Profiler | Enterprise Security Measured",
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    },
+    "info/privacy-policy": {
+      // entry for the page
+      entry: "src/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist
+      filename: "info/privacy-policy.html",
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "ES Profiler | Enterprise Security Measured",
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    },
+    "info/legal-notice": {
+      // entry for the page
+      entry: "src/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist
+      filename: "info/legal-notice.html",
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "ES Profiler | Enterprise Security Measured",
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    },
+    "404": {
+      // entry for the page
+      entry: "src/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist
+      filename: "404.html",
       // when using title option,
       // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
       title: "ES Profiler | Enterprise Security Measured",
