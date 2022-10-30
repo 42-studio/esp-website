4bf483c7f52580e29703014b75fa476221f60369
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2022-10-30 16:29:51 +0000

    Testing MPA

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,34 @@
 module.exports = {
+  pages: {
+    index: {
+      // entry for the page
+      entry: "src/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist/index.html
+      filename: "index.html",
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "ES Profiler | Enterprise Security Measured",
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    },
+    "schedule-demo": {
+      // entry for the page
+      entry: "src/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist/index.html
+      filename: "schedule-demo.html",
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "ES Profiler | Enterprise Security Measured",
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    }
+  },
 
   css: {
     loaderOptions: {
@@ -9,4 +39,4 @@ module.exports = {
   },
 
   lintOnSave: false
-}
+};
