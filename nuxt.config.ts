046d394459be7fb02eafd70e2a2fe6b7b37acd7a
Marc Newton <marc.newton@esprofiler.com>
2022-12-03 16:09:43 +0000

    Icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,16 +12,6 @@ export default defineNuxtConfig({
         },
         head: {
             title: 'ESPROFILER | Continuous Security Investment Validation',
-            meta: [
-                {
-                    charset: 'utf-8'
-                },
-                {
-                    hid: 'description',
-                    name: 'description',
-                    content: 'An enterprise platform for continuous assurance of security investments against threats.'
-                }
-            ],
             link: [
                 {
                     rel: 'preconnect',
@@ -35,6 +25,54 @@ export default defineNuxtConfig({
                 {
                     rel: 'stylesheet',
                     href: 'https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'
+                },
+                {
+                    rel: 'apple-touch-icon',
+                    sizes: '180x180',
+                    href: '/icons/apple-touch-icon.png'
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '32x32',
+                    href: '/icons/favicon-32x32.png'
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '16x16',
+                    href: '/icons/favicon-16x16.png'
+                },
+                {
+                    rel: 'mask-icon',
+                    href: '/icons/safari-pinned-tab.svg',
+                    color: '#bd3dde'
+                },
+                {
+                    rel: 'shortcut icon',
+                    href: '/icons/favicon.ico'
+                }
+            ],
+            meta: [
+                {
+                    charset: 'utf-8'
+                },
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'An enterprise platform for continuous assurance of security investments against threats.'
+                },
+                {
+                    name: 'msapplication-TileColor',
+                    content: '#da532c'
+                },
+                {
+                    name: 'msapplication-config',
+                    content: '/icons/browserconfig.xml'
+                },
+                {
+                    name: 'theme-color',
+                    content: '#ffffff'
                 }
             ]
         }
